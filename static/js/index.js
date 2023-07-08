b4898a65c4842e1a266c9e9755a3915fda227d42
Akash Kumar <akash.kumar@vmock.com>
2023-07-08 17:50:06 +0530

    final updates

--- static/js/index.js
+++ static/js/index.js
@@ -19,6 +19,9 @@ function setInterpolationImage(i) {
   $('#interpolation-image-wrapper').empty().append(image);
 }
 
+function Alert() {
+  alert("Code will be available soon!!");
+}
 
 $(document).ready(function() {
     // Check for click events on the navbar burger icon
@@ -75,4 +78,22 @@ $(document).ready(function() {
 
     bulmaSlider.attach();
 
+    for(let i=0; i<4; i++){
+    $(`img[carousal-img-${i+1}]`).addClass('img-enlargable').click(function(){
+      var src = $(this).attr('src');
+      $('<div>').css({
+          background: '#ffffff url('+src+') no-repeat center',
+          backgroundSize: 'contain',
+          width:'100%', height:'100%',
+          position:'fixed',
+          zIndex:'10000',
+          top:'0', left:'0',
+          cursor: 'zoom-out'
+      }).click(function(){
+          $(this).remove();
+      }).appendTo('body');
+  });
+}
+
 })
+
